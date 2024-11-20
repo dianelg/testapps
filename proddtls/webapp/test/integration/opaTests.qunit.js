e4f3ac495053e56897321f5e27e05a188b81aee2
Diane Guevarra <guevarradiane4@gmail.com>
2024-11-20 11:27:22 +0000

    Initial Commit

---
+++ proddtls/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/proddtls/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ proddtls/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/proddtls/test/unit/controller/Products.controller"
+], function () {
+	"use strict";
+});
